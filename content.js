a50ee05605e704369ec312ad832f7ba96f7d75e2
winstxnhdw <winstxnhdw@gmail.com>
2022-02-03 20:11:25 +0800

    injects script for every mutation

--- content.js
+++ content.js
@@ -1,7 +1,20 @@
-const hidden_elements = document.getElementsByClassName('hidden')
-hidden_elements[1].classList.remove('hidden')
-hidden_elements[1].classList.remove('hidden')
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 
-const old_max_length = 2000
-const new_max_length = 10000
-document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = new_max_length
+var observer = new MutationObserver(() => {
+    const hidden_elements = document.getElementsByClassName('hidden')
+    hidden_elements[1].classList.remove('hidden')
+    hidden_elements[1].classList.remove('hidden')
+
+    const old_max_length = 2000
+    const new_max_length = 10000
+    document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = new_max_length
+})
+
+observer.observe(document, {
+    childList: true,
+    attributes: true,
+    characterData: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+})
