8e32a968a329d93f9b21819471743a5065f48e60
winstxnhdw <winstxnhdw@gmail.com>
2022-02-03 19:54:18 +0800

    update

--- content.js
+++ content.js
@@ -2,6 +2,6 @@ const hidden_elements = document.getElementsByClassName('hidden')
 hidden_elements[1].classList.remove('hidden')
 hidden_elements[1].classList.remove('hidden')
 
-const old_max_length = '2000'
-const new_max_length = '10000'
-document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = parseInt(new_max_length)
+const old_max_length = 2000
+const new_max_length = 10000
+document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = new_max_length
