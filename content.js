b35312239de600fc0905bf66a4cb6908ba6e67d2
winstxnhdw <winstxnhdw@gmail.com>
2022-02-17 19:18:44 +0800

    2 for spaces

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 
 var observer = new MutationObserver(() => {
-    for (let element of document.getElementsByClassName('hidden')) {
-        if (element.id === '829681c8-0152-c55f-8cd7-065b9125bf04' || element.id === '5deb06ee-6c5e-a776-3dd0-1d8d9cd8b1e5') {
-            element.classList.remove('hidden')
-        }
+  for (let element of document.getElementsByClassName('hidden')) {
+    if (element.id === '829681c8-0152-c55f-8cd7-065b9125bf04' || element.id === '5deb06ee-6c5e-a776-3dd0-1d8d9cd8b1e5') {
+      element.classList.remove('hidden')
     }
+  }
 
-    const old_max_length = 2000
-    const new_max_length = 10000
-    document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = new_max_length
+  const old_max_length = 2000
+  const new_max_length = 10000
+  document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = new_max_length
 })
 
 observer.observe(document, {
-    childList: true,
-    attributes: true,
-    subtree: true
+  childList: true,
+  attributes: true,
+  subtree: true
 })
