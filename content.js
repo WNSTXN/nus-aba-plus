c41c62465467e4acb5eb4c11d3857e4d5c3591ea
winstxnhdw <winstxnhdw@gmail.com>
2022-02-17 19:23:51 +0800

    better performance

--- content.js
+++ content.js
@@ -1,16 +1,27 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 
+const old_max_length = 2000
+const new_max_length = 10000
+
 var observer = new MutationObserver(() => {
+  setInterval(() => {
+    injection()
+  }, 500)
+
+  setInterval(() => {
+    observer.disconnect()
+  }, 10000)
+})
+
+function injection() {
   for (let element of document.getElementsByClassName('hidden')) {
     if (element.id === '829681c8-0152-c55f-8cd7-065b9125bf04' || element.id === '5deb06ee-6c5e-a776-3dd0-1d8d9cd8b1e5') {
       element.classList.remove('hidden')
     }
   }
 
-  const old_max_length = 2000
-  const new_max_length = 10000
   document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = new_max_length
-})
+}
 
 observer.observe(document, {
   childList: true,
