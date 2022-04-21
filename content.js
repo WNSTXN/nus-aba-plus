b95ab8ba2b4010b86f62acba46823efe238cb9b0
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-04-21 14:27:54 +0800

    refactor

--- content.js
+++ content.js
@@ -1,7 +1,21 @@
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver
-
 const old_max_length = 2000
 const new_max_length = 10000
+
+function injection() {
+  for (let element of document.getElementsByClassName('hidden')) {
+    if (
+      element.id === '829681c8-0152-c55f-8cd7-065b9125bf04' ||
+      element.id === '5deb06ee-6c5e-a776-3dd0-1d8d9cd8b1e5'
+    ) {
+      element.classList.remove('hidden')
+    }
+  }
+
+  document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength =
+    new_max_length
+}
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 
 var observer = new MutationObserver(() => {
   setInterval(() => {
@@ -13,18 +27,8 @@ var observer = new MutationObserver(() => {
   }, 10000)
 })
 
-function injection() {
-  for (let element of document.getElementsByClassName('hidden')) {
-    if (element.id === '829681c8-0152-c55f-8cd7-065b9125bf04' || element.id === '5deb06ee-6c5e-a776-3dd0-1d8d9cd8b1e5') {
-      element.classList.remove('hidden')
-    }
-  }
-
-  document.querySelectorAll(`[maxlength="${old_max_length}"]`)[0].maxLength = new_max_length
-}
-
 observer.observe(document, {
   childList: true,
   attributes: true,
-  subtree: true
+  subtree: true,
 })
